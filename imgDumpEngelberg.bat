3598d4c5ad3c3e17de88b5bcf7d47fdd5ba125a7
Kristian <kristian.enge@snowsoftware.com>
2018-01-17 13:12:31 +0100

    fixed script

--- imgDumpEngelberg.bat
+++ imgDumpEngelberg.bat
@@ -1,21 +1,18 @@
 @echo off
 
-
-set YYYY=%DATE:~10,4%
-set MM=%DATE:~4,2%
-set DD=%DATE:~7,2%
-
 set HH=%TIME: =0%
 set HH=%HH:~0,2%
 set MI=%TIME:~3,2%
 set SS=%TIME:~6,2%
 set FF=%TIME:~9,2%
 
+for /f "skip=1" %%x in ('wmic os get localdatetime') do if not defined MyDate set MyDate=%%x
+set today=%MyDate:~0,4%-%MyDate:~4,2%-%MyDate:~6,2%_%HH%%MI%%SS%
 
 SET folder=Dump/
 SET picName=Img
 SET date=%YYYY%%MM%%DD%_%HH%%MI%%SS%
-SET FullName=%folder%%picName%_%date%.jpg 
+SET FullName=%folder%%picName%_%today%.jpg 
 
 echo %FullName%
 
